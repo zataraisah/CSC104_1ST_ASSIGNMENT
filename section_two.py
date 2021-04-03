a164386d618e34c4ea1496aa708085fea983296e
Fatima Muhammad <81846014+codefalim@users.noreply.github.com>
2021-04-03 13:54:00 +0100

    Add files via upload
    CSC104 FIRST ASSIGNMENT SECTION ONE AND TWO

---
+++ section_two.py
@@ -0,0 +1,84 @@
+print("____________________________QUESTION 1____________________________")
+# Write a short Python code to display the name of your name & department
+
+print('NAME: FATIMA MUHAMMAD \nDEPARTMENT: COMPUTER SCIENCE')
+
+print("____________________________QUESTION 2____________________________")
+# Assign the string "Boo!" to the variable scare and print the variable
+
+scare = "Boo!"
+print(scare)
+
+
+print("____________________________QUESTION 3____________________________")
+# Add two variables and assign the result to a third variable. print all the variable names
+
+var_1 = 5
+
+var_2 = 6
+
+var_3 = var_1 + var_2
+
+print("Variable one is: {}".format(var_1))
+print("Variable two is: {}".format(var_2))
+print("Variable three is: {}".format(var_3))
+
+
+print("____________________________QUESTION 4____________________________")
+
+# Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
+
+integer_1 = 12
+
+integer_2 = .5
+
+result = integer_1/integer_2
+
+print("First integer: {}".format(integer_1))
+print("Second integer: {}".format(integer_2))
+print("Result: {} ".format(result))
+
+
+print("____________________________QUESTION 5____________________________")
+# Assign the sum of two numbers to a variable and print the variable.
+
+num_1 = 10
+
+num_2 = 20
+
+_sum = num_1 + num_2
+
+print("First number: {} \nSecond number: {} \nSum: {}".format(num_1, num_2, _sum))
+
+
+print("____________________________QUESTION 6____________________________")
+
+# Write the code that finds the remainder when 8 is divided by 3
+dividend = 8
+divisor = 3
+remainder = dividend % divisor
+
+print("Dividend = {} \nDivisor = {} \nRemainder = {}".format(dividend, divisor, remainder))
+
+print("____________________________QUESTION 7____________________________")
+
+# Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2.
+# Then multiply the first result by the second result. Print the result. x = 2 * 4 * 4 + 2
+
+x = (2*4)*(4+2)
+
+print("x = (2*4)*(4+2): {}".format(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
